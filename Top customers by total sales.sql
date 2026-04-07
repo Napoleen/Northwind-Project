110d4feccbf893c74cd70febe8170a4c46dc2792
Naptime <hennesseynate@gmail.com>
2026-04-06 20:45:37 -0400

    Test.sql

---
+++ Top customers by total sales.sql
@@ -0,0 +1,22 @@
+/* Top customers by total sales */
+1
+select  * FROM (
+    SELECT 
+        c.CustomerID,
+        c.CompanyName,
+        SUM(od.UnitPrice * od.Quantity) AS TotalSales,
+        COUNT(od.ORDERiD) AS OrderCount,
+        AVG(od.UnitPrice * od.Quantity) AS AverageOrderValue
+        
+    FROM 
+        Customers c 
+    JOIN 
+        Orders o ON c.CustomerID = o.CustomerID
+    JOIN 
+        [Order Details] od ON o.OrderID = od.OrderID
+    GROUP BY 
+        c.CustomerID, c.CompanyName
+) AS CustomerSales 
+ order by TotalSales desc;
+
+
